40efbc0fa7941220d73f35ed22ee10b801e1c688
Jay Buffington <jaybuffington@gmail.com>
2010-10-25 07:07:21 +0000

    fleshed out entering the joot.  mount home dir, setup env, etc

--- lib/Joot.pm
+++ lib/Joot.pm
@@ -6,6 +6,7 @@ use warnings;
 use vars '$VERSION';
 $VERSION = "0.0.1";
 
+use English '-no_match_vars';
 use File::Copy  ();
 use IPC::Cmd    ();
 use Joot::Image ();
@@ -36,28 +37,100 @@ sub new {
     return $self;
 }
 
+# usage:
+# chroot( "foo" );
+# chroot( "foo", { user => "root", cmd => "adduser jaybuff" } );
 sub chroot {    ## no critic qw(Subroutines::ProhibitBuiltinHomonyms)
-    my $self = shift;
+    my $self      = shift;
     my $joot_name = shift || die "missing joot name to chroot into\n";
+    my $args      = shift;
 
     my $joot_dir = $self->joot_dir($joot_name);
-
-    #TODO check to see if it's already connected
-    my $device = nbd_connect("$joot_dir/disk.qcow2");
 
     my $mnt = "$joot_dir/mnt";
     mkpath($mnt);
 
+    #TODO check to see if it's already connected
+    my $device = nbd_connect("$joot_dir/disk.qcow2");
+    push @{ $self->{connected_devices} }, $device;    # see cleanup()
+
     #TODO some images have partitions (mount ${device}p1 etc)
     run( bin("mount"), $device, $mnt );
+    push @{ $self->{mount_points} }, $mnt;            # see cleanup()
 
-    # start the user's shell inside this chroot
-    my $shell = (getpwent)[8];
-    system( bin("chroot"), $mnt, $shell );
+    # allow the user to specify the user to enter the chroot as
+    my $user = $args->{user} || getpwuid($REAL_USER_ID);
+    my $real_homedir = ( getpwnam($user) )[7];
 
-    # when the user exits the joot unmount and disconnect it
-    run( bin("umount"), $mnt );
-    nbd_disconnect($device);
+    # mount /proc, /sys, /dev and the user's home dir
+    #TODO support automount setting in config file
+    #TODO support passed in mount points in $args
+    #TODO support readonly home dirs (and mount points)
+    foreach my $dir ( $real_homedir, qw(/proc /sys /dev) ) {
+        my $target = "$mnt/$dir";
+        mkpath($target);
+        run( bin("mount"), "--bind", $dir, $target );
+        push @{ $self->{mount_points} }, $target;    # see cleanup()
+    }
+
+    # we have to fork here because we need to chroot for the getpwname to work
+    # properly.  if we didn't fork there's no way to "exit" the chroot (afaik)
+    # we have to exit it so we can clean up afterwards (umount, disconnect, etc)
+    my $pid = fork();
+    if ( $pid == 0 ) {
+        chroot($mnt);
+        my ( $uid, $gid, $homedir, $shell ) = ( getpwnam($user) )[ 2, 3, 7, 8 ];
+
+        # check that the user exists in the chroot
+        if ( !defined $uid ) {
+            FATAL "User $user doesn't exist inside joot '$joot_name'";
+            FATAL "Try running \"$PROGRAM_NAME $joot_name --user root --cmd 'adduser $user'\" to create the account";
+            die "\n";
+        }
+
+        # chdir to user's $homedir which we mounted above
+        if ( $real_homedir ne $homedir ) {
+            WARN "${user}'s home dir in chroot is different than home dir outside of chroot.";
+            WARN "Mounted home dir in $real_homedir, but chdir'ing to $homedir";
+        }
+        chdir($homedir);
+
+        # set effective/real gid and uid to the uid/gid of the user we're
+        # entering the chroot as
+        # this is basically setuid/setgid
+        ( $REAL_USER_ID,  $EFFECTIVE_USER_ID )  = ( $uid, $uid );
+        ( $REAL_GROUP_ID, $EFFECTIVE_GROUP_ID ) = ( $gid, $gid );
+
+        # clean up %ENV
+        # set this env var so the user has a way to tell what joot they're in
+        $ENV{JOOT_NAME} = $joot_name;
+        foreach my $env_var (qw( SUDO_COMMAND SUDO_GID SUDO_UID SUDO_USER )) {
+            delete $ENV{$env_var};
+        }
+        $ENV{LOGNAME} = $ENV{USERNAME} = $ENV{USER} = $user;
+        $ENV{HOME} = $homedir;
+
+        # the user may have passed in this command to run instead of their shell
+        if ( my $cmd = $args->{cmd} ) {
+            exec($cmd) or die "failed to exec $cmd: $!\n";
+        }
+
+        # start the user's shell inside this chroot
+        if ( !-x $shell ) {
+            FATAL "can't execute $shell.  use \"$PROGRAM_NAME $joot_name --cmd 'chsh /bin/sh'\" to fix";
+            die "\n";
+        }
+
+        #TODO make the user's shell a login shell so .bashrc, etc are executed
+        exec($shell) or die "Failed to exec shell $shell: $!\n";
+    }
+
+    # when the fork'ed process exits we should clean up rather than waiting for
+    # the objects destructor
+    # also if we don't wait here the user will have two shells running at once
+    # when joot exists.
+    waitpid( $pid, 0 );
+    cleanup();
 
     return;
 }
@@ -145,7 +218,7 @@ sub images {
             1;
         } or do {
             WARN "Failed getting images from $url";
-            DEBUG $@;
+            DEBUG $EVAL_ERROR;
           }
     }
 
@@ -172,7 +245,9 @@ sub list {
 
 sub delete {    ## no critic qw(Subroutines::ProhibitBuiltinHomonyms Subroutines::RequireArgUnpacking)
     my $self  = shift;
+    my $args  = pop;
     my @joots = @_;
+
     if ( !@joots ) {
         die "missing joot name to delete\n";
     }
@@ -211,8 +286,26 @@ sub rename {    ## no critic qw(Subroutines::ProhibitBuiltinHomonyms)
     return;
 }
 
-# TODO
-# make the qcow disk size something bigger than 10GB when it's installed
+sub cleanup {
+    my $self = shift;
+
+    # we do both these arrays in reverse order that they were created
+    # this is because they might depend on the earlier ones
+    while ( my $mnt = pop @{ $self->{mount_points} } ) {
+        run( bin("umount"), $mnt );
+    }
+
+    while ( my $device = pop @{ $self->{connected_devices} } ) {
+        nbd_disconnect($device);
+    }
+
+    return;
+}
+
+sub DESTROY {
+    my $self = shift;
+    return $self->cleanup();
+}
 
 1;
 
